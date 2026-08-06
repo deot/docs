cab7f9187cae8c4194f28f2f1034f022d018c8af
Mr.Z <zrd0921@qq.com>
2026-08-06 09:41:43 +0800

    docs: site, align sidebar schema and simplify sample routes

---
+++ site/en-US/components/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="v-index">
+		<router-link to="/zh-CN/components/installation">
+			<h1>Hello @deot/docs - Quick Start</h1>
+		</router-link>
+	</div>
+</template>
+
+<style lang="scss">
+.v-index {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex: 1;
+	min-height: 600px;
+}
+</style>
